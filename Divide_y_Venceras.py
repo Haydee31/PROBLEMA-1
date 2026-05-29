8860b3346cfa6e6983ea35151ab645f2bbe6ea65
Haydee Delgado <dhelgado723@gmail.com>
2026-05-29 18:41:35 -0500

    Creamos Fila y matriz

--- Divide_y_Venceras.py
+++ Divide_y_Venceras.py
@@ -5,7 +5,15 @@ def crear_fila(inicio, fin):
     mitad = (inicio + fin) // 2
     izquierda = crear_fila(inicio, mitad)      
     derecha   = crear_fila(mitad + 1, fin)    
-    return izquierda + derecha                 
-
+    return izquierda + derecha    
+             
+def crear_matriz(fila_ini, fila_fin, n):
+    if fila_ini == fila_fin:                            
+        return [crear_fila(0, n - 1)]
+    mitad = (fila_ini + fila_fin) // 2
+    superior = crear_matriz(fila_ini, mitad,     n)      
+    inferior  = crear_matriz(mitad + 1, fila_fin, n)     
+    return superior + inferior                           
+ 
 
  
